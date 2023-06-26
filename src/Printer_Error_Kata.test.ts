6007bdf0d13e00ffbffcc64f0ca44a75e4d7bace
Your Name <you@example.com>
2023-06-26 18:28:51 +0100

    updated test description

--- src/Printer_Error_Kata.test.ts
+++ src/Printer_Error_Kata.test.ts
@@ -1,9 +1,9 @@
 import printerError from "./Printer_Error_Kata";
 
-test("sum adds two numbers", () => {
+test("check in a string how many that are not A-M", () => {
   expect(printerError("aaabbbbhaijjjm")).toBe("0/14");
 });
 
-test("sum adds two numbers cancelling", () => {
+test("check in a string how many that are not A-M", () => {
   expect(printerError("aaaxbbbbyyhwawiwjjjwwm")).toBe("8/22");
 });
